6686d6bcfa5b6b2f3c65b755ba1f6b78fcb1eb4f
Dariusz Donimirski <dariusz.donimirski@gmail.com>
2025-04-28 21:49:41 +0200

    add dumy storage

--- services/storage/src/main.rs
+++ services/storage/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Starting service...");
+use axum::{Router, routing::{get, post, put, delete}};
+use axum_server::Server;
+use std::net::SocketAddr;
+
+mod routes;
+use routes::*;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/companies", get(get_companies).post(create_company))
+        .route("/companies/:id", get(get_company).put(update_company).delete(delete_company))
+        .route("/customers", get(get_customers).post(create_customer))
+        .route("/customers/:id", get(get_customer).put(update_customer).delete(delete_customer));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
+    println!("🔌 storage-service running on http://{}", addr);
+    Server::bind(addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
--- services/storage/src/routes.rs
+++ services/storage/src/routes.rs
@@ -1,5 +1,5 @@
 use axum::{Json, extract::Path};
-use common::models::{Company, Customer};
+use common::models::invoice::{Company}
 
 pub async fn get_companies() -> Json<Vec<Company>> {
     Json(vec![]) // TODO: Load from DB
@@ -7,16 +7,7 @@ pub async fn get_companies() -> Json<Vec<Company>> {
 
 pub async fn get_company(Path(id): Path<i64>) -> Json<Company> {
     Json(Company {
-        id,
-        nazwa: "Example".into(),
-        nip: "1234567890".into(),
-        enip: Some("PL1234567890".into()),
-        numer_konta: "12345678901234567890123456".into(),
-        email: Some("firma@example.com".into()),
-        tel: Some("+48123456789".into()),
-        adres_ulica: "Ulica".into(),
-        adres_kod_pocztowy: "00-000".into(),
-        adres_miasto: "Miasto".into(),
+
     })
 }
 
@@ -32,31 +23,28 @@ pub async fn delete_company(Path(_id): Path<i64>) -> Json<&'static str> {
     Json("Company deleted")
 }
 
-pub async fn get_customers() -> Json<Vec<Customer>> {
+pub async fn get_customers() -> Json<Vec<Company>> {
     Json(vec![]) // TODO: Load from DB
 }
 
-pub async fn get_customer(Path(id): Path<i64>) -> Json<Customer> {
-    Json(Customer {
-        id,
-        nazwa: "Klient".into(),
-        nip: "9876543210".into(),
-        email: Some("klient@example.com".into()),
-        tel: Some("+48111222333".into()),
-        adres_ulica: "Klienta 5".into(),
-        adres_kod_pocztowy: "11-111".into(),
-        adres_miasto: "Klientowo".into(),
+pub async fn get_customer(Path(id): Path<i64>) -> Json<Company> {
+    Json(Company {
+        nazwa: String,
+        nip: String,
+        enip: String,
+        numer_konta: String,
+        adres: Address,
     })
 }
 
-pub async fn create_customer(Json(_payload): Json<Customer>) -> Json<&'static str> {
-    Json("Customer created")
+pub async fn create_customer(Json(_payload): Json<Company>) -> Json<&'static str> {
+    Json("Company created")
 }
 
-pub async fn update_customer(Path(_id): Path<i64>, Json(_payload): Json<Customer>) -> Json<&'static str> {
-    Json("Customer updated")
+pub async fn update_customer(Path(_id): Path<i64>, Json(_payload): Json<Company>) -> Json<&'static str> {
+    Json("Company updated")
 }
 
 pub async fn delete_customer(Path(_id): Path<i64>) -> Json<&'static str> {
-    Json("Customer deleted")
+    Json("Company deleted")
 }
